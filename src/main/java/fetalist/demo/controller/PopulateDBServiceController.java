036e5d581df351bb84d410a011d9ff85876552ec
romain22222 <romrom22222@hotmail.fr>
2023-04-04 10:09:52 +0200

    feat: error handler /populate

--- src/main/java/fetalist/demo/controller/PopulateDBServiceController.java
+++ src/main/java/fetalist/demo/controller/PopulateDBServiceController.java
@@ -2,9 +2,12 @@ package fetalist.demo.controller;
 
 import fetalist.demo.service.PopulateDBService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/populate")
@@ -13,7 +16,8 @@ public class PopulateDBServiceController {
     private PopulateDBService populateDBService;
 
     @PostMapping("/")
-    public void fillDatabaseWithJson() {
-        populateDBService.fillDatabaseWithJson();
+    public ResponseEntity<String> fillDatabaseWithJson() {
+        String s = populateDBService.fillDatabaseWithJson();
+        return ResponseEntity.status(Objects.equals(s, "OK") ? 200 : 400).body(s);
     }
 }
