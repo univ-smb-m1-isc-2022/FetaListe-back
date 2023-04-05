603ee533faca9debadb159be96bcfad3be81760b
romain22222 <romrom22222@hotmail.fr>
2023-04-05 09:52:09 +0200

    FOUTU TEST

--- src/test/java/fetalist/demo/controller/ReceipeControllerTest.java
+++ src/test/java/fetalist/demo/controller/ReceipeControllerTest.java
@@ -7,42 +7,42 @@ import fetalist.demo.model.Unit;
 import fetalist.demo.repository.IngredientRepository;
 import fetalist.demo.repository.UnitRepository;
 import fetalist.demo.service.ReceipeService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
 import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
 public class ReceipeControllerTest {
 
     private MockMvc mockMvc;
 
-    @Mock
     private ReceipeService receipeService;
 
-    @Autowired
+    private UnitRepository unitRepository;
     private IngredientRepository ingredientRepository;
 
-    @Autowired
-    private UnitRepository unitRepository;
+    // Tests
 
-    @BeforeAll
-    public void setUp() {
+    @BeforeEach
+    public void setup() {
+        receipeService = mock(ReceipeService.class);
+        mockMvc = standaloneSetup(new ReceipeController(receipeService)).build();
+        unitRepository = mock(UnitRepository.class);
+        ingredientRepository = mock(IngredientRepository.class);
+
+
         // Créer et sauvegarder des ingrédients
         Ingredient ingredient1 = new Ingredient();
         ingredient1.setName("Tomate");
@@ -62,19 +62,10 @@ public class ReceipeControllerTest {
         unitRepository.save(unit2);
     }
 
-    // Tests
-
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        ReceipeController receipeController = new ReceipeController(receipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(receipeController).build();
-    }
-
     @Test
     public void testCreateReceipe() throws Exception {
-        Ingredient i = ingredientRepository.findBy(Example.of(Ingredient.builder().name("Tomate").build()), FluentQuery.FetchableFluentQuery::first).orElseThrow();
-        Unit u = unitRepository.findBy(Example.of(Unit.builder().name("mL").build()), FluentQuery.FetchableFluentQuery::first).orElseThrow();
+        when(ingredientRepository.findBy(Example.of(Ingredient.builder().idIngredient(1L).build()), FluentQuery.FetchableFluentQuery::first)).thenReturn(Optional.of(Ingredient.builder().idIngredient(1L).name("Tomate").build()));
+        when(unitRepository.findBy(Example.of(Unit.builder().idUnit(2L).build()), FluentQuery.FetchableFluentQuery::first)).thenReturn(Optional.of(Unit.builder().idUnit(2L).name("mL").build()));
         CreateReceipeBody receipeBody = CreateReceipeBody.builder()
                 .name("Pizza Margherita")
                 .categoryName("Italian")
@@ -83,8 +74,8 @@ public class ReceipeControllerTest {
                 .instructions(List.of("Faire la pate", "Mettre la sauce", "Mettre le fromage", "Faire cuire"))
                 .ri(List.of(
                         ReceipeIngredient.builder()
-                                .ingredient(i)
-                                .unit(u)
+                                .ingredient(Ingredient.builder().idIngredient(1L).build())
+                                .unit(Unit.builder().idUnit(2L).build())
                                 .quantity(51.4).build()))
                 .build();
         when(receipeService.createReceipe(receipeBody)).thenReturn(true);
@@ -103,11 +94,11 @@ public class ReceipeControllerTest {
                                     "ri": [
                                         {
                                                 "ingredient": {
-                                                    "idIngredient":"""+ i.getIdIngredient() +"""
+                                                    "idIngredient": 1
                                                 },
                                                 "quantity": 51.4,
                                                 "unit": {
-                                                    "idUnit":""" + u.getIdUnit() + """
+                                                    "idUnit": 2
                                                 }
                                         }
                                     ]
