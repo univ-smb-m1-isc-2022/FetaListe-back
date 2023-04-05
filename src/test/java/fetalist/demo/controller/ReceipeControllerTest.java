f33feca8922a4858cb7c22c95f5c91e5040d2780
romain22222 <romrom22222@hotmail.fr>
2023-04-05 08:26:43 +0200

    tu test

--- src/test/java/fetalist/demo/controller/ReceipeControllerTest.java
+++ src/test/java/fetalist/demo/controller/ReceipeControllerTest.java
@@ -1,16 +1,22 @@
-/*
 package fetalist.demo.controller;
 
 import fetalist.demo.bodies.CreateReceipeBody;
 import fetalist.demo.model.Ingredient;
 import fetalist.demo.model.ReceipeIngredient;
 import fetalist.demo.model.Unit;
+import fetalist.demo.repository.IngredientRepository;
+import fetalist.demo.repository.UnitRepository;
 import fetalist.demo.service.ReceipeService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,12 +27,42 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ReceipeControllerTest {
 
     private MockMvc mockMvc;
 
     @Mock
     private ReceipeService receipeService;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    private UnitRepository unitRepository;
+
+    @BeforeAll
+    public void setUp() {
+        // Créer et sauvegarder des ingrédients
+        Ingredient ingredient1 = new Ingredient();
+        ingredient1.setName("Tomate");
+        ingredientRepository.save(ingredient1);
+
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setName("Fromage");
+        ingredientRepository.save(ingredient2);
+
+        // Créer et sauvegarder des unités
+        Unit unit1 = new Unit();
+        unit1.setName("g");
+        unitRepository.save(unit1);
+
+        Unit unit2 = new Unit();
+        unit2.setName("mL");
+        unitRepository.save(unit2);
+    }
+
+    // Tests
 
     @BeforeEach
     public void setup() {
@@ -37,6 +73,8 @@ public class ReceipeControllerTest {
 
     @Test
     public void testCreateReceipe() throws Exception {
+        Ingredient i = ingredientRepository.findBy(Example.of(Ingredient.builder().name("Tomate").build()), FluentQuery.FetchableFluentQuery::first).orElseThrow();
+        Unit u = unitRepository.findBy(Example.of(Unit.builder().name("mL").build()), FluentQuery.FetchableFluentQuery::first).orElseThrow();
         CreateReceipeBody receipeBody = CreateReceipeBody.builder()
                 .name("Pizza Margherita")
                 .categoryName("Italian")
@@ -45,8 +83,8 @@ public class ReceipeControllerTest {
                 .instructions(List.of("Faire la pate", "Mettre la sauce", "Mettre le fromage", "Faire cuire"))
                 .ri(List.of(
                         ReceipeIngredient.builder()
-                                .ingredient(Ingredient.builder().idIngredient(7L).build())
-                                .unit(Unit.builder().idUnit(5L).build())
+                                .ingredient(i)
+                                .unit(u)
                                 .quantity(51.4).build()))
                 .build();
         when(receipeService.createReceipe(receipeBody)).thenReturn(true);
@@ -65,18 +103,17 @@ public class ReceipeControllerTest {
                                     "ri": [
                                         {
                                                 "ingredient": {
-                                                    "idIngredient": 7
+                                                    "idIngredient":"""+ i.getIdIngredient() +"""
                                                 },
                                                 "quantity": 51.4,
                                                 "unit": {
-                                                    "idUnit": 5
+                                                    "idUnit":""" + u.getIdUnit() + """
                                                 }
                                         }
                                     ]
                                 }""")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$").value(true));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
-}*/
+}
